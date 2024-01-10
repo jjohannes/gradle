bd3ded57b942d4fa3c1f1f4f8209b4c1ae508ac6
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-01-10 09:59:24 +0100

    [8.4 PATCH] 8.4 patched with more parallelism (as 8.4.101)

--- gradle-bin/bin/gradle.bat
+++ gradle-bin/bin/gradle.bat
@@ -34,7 +34,7 @@ set APP_HOME=%DIRNAME%..
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m" "-javaagent:%APP_HOME%/lib/agents/gradle-instrumentation-agent-8.4.100.jar"
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m" "-javaagent:%APP_HOME%/lib/agents/gradle-instrumentation-agent-8.4.101.jar"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -68,7 +68,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\gradle-launcher-8.4.100.jar
+set CLASSPATH=%APP_HOME%\lib\gradle-launcher-8.4.101.jar
 
 
 @rem Execute Gradle
